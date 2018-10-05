d545c67e15b90c6080688940e1f109908c8d2e56
Bubly04 <sayma.aryan@gmail.com>
2018-10-04 18:36:44 -0700

    We are students

--- Aryan/src/com/icici/loans/home/StudentLoan.java
+++ Aryan/src/com/icici/loans/home/StudentLoan.java
@@ -3,6 +3,7 @@ package com.icici.loans.home;
 public class StudentLoan {
 
 	public static void main(String[] args) {
+		System.out.println("we are students");
 		// TODO Auto-generated method stub
 
 	}
